363fc2a6c1d4539d1de51ebbebbeabf7b6a0a1b7
RobbingDaHood <he@nykredit.dk>
2021-05-26 00:16:10 +0200

    Naive implementation of postCustomer

--- src/main/java/com/example/coolshop/customer/api/CustomerExposure.java
+++ src/main/java/com/example/coolshop/customer/api/CustomerExposure.java
@@ -3,9 +3,7 @@ package com.example.coolshop.customer.api;
 import com.example.coolshop.customer.api.mappers.CustomerMapper;
 import com.example.coolshop.customer.api.representation.CustomerRepresentation;
 import com.example.coolshop.customer.domain.CustomerService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CustomerExposure {
@@ -19,5 +17,10 @@ public class CustomerExposure {
     @GetMapping("/customers/{customerId}")
     public CustomerRepresentation getCustomer(@PathVariable(value = "customerId") String customerId) {
         return CustomerMapper.mapFromDomain(customerService.getCustomer(customerId));
+    }
+
+    @PostMapping("/customers")
+    public void postCustomer(@RequestBody CustomerRepresentation customerRepresentation) {
+        customerService.registerCustomer(CustomerMapper.mapToDomain(customerRepresentation));
     }
 }
