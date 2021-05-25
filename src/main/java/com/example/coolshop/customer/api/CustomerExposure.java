20be025d47bf3c46ce68e2b339fc69dca4735c8e
RobbingDaHood <he@nykredit.dk>
2021-05-25 23:43:50 +0200

    First unit test in CustomerExposureTest

--- src/main/java/com/example/coolshop/customer/api/CustomerExposure.java
+++ src/main/java/com/example/coolshop/customer/api/CustomerExposure.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CustomerExposure {
 
-    private CustomerService customerService;
+    private final CustomerService customerService;
 
     public CustomerExposure(CustomerService customerService) {
         this.customerService = customerService;
