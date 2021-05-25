be9a583e9494549ec560137ecba2b1734d644952
RobbingDaHood <he@nykredit.dk>
2021-05-25 22:19:07 +0200

    initial commit

---
+++ src/test/java/com/example/coolshopapi/CoolshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coolshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
